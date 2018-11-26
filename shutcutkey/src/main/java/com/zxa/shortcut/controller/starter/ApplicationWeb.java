8e2ecb43295dbe84975fb7af60e910b8c80041c8
xin_anzhang <zxadgg@gmail.com>
2018-11-26 21:21:50 +0800

    maven部分依赖

---
+++ shutcutkey/src/main/java/com/zxa/shortcut/controller/starter/ApplicationWeb.java
@@ -0,0 +1,10 @@
+package com.zxa.shortcut.controller.starter;
+
+/**
+ * @ClassName: ApplicationWeb
+ * @Description: //TODO
+ * @Author: zhangxin_an
+ * @CreateDate: 2018/11/26 21:15
+ */
+public class ApplicationWeb {
+}
