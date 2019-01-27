dd4ccf95fa52370291480fc075d6ecb436df89d8
xin_anzhang <zxadgg@gmail.com>
2019-01-27 21:17:44 +0800

    跨域

--- shortcutkey/src/main/java/com/zxa/shortcut/config/MyConfiguration.java
+++ shortcutkey/src/main/java/com/zxa/shortcut/config/MyConfiguration.java
@@ -1,11 +1,10 @@
 package com.zxa.shortcut.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @ClassName: MyConfiguration
@@ -14,20 +13,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @CreateDate: 2019/1/27 21:07
  */
 @Configuration
-public class MyConfiguration extends WebMvcConfigurerAdapter {
-	@Bean
-	public CorsFilter corsFilter() {
-		CorsConfiguration config = new CorsConfiguration();
-		config.addAllowedOrigin("*");
-		config.setAllowCredentials(true);
-		config.addAllowedMethod("*");
-		config.addAllowedHeader("*");
-		config.addExposedHeader("*");
-
-		UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
-		configSource.registerCorsConfiguration("/**", config);
-
-		return new CorsFilter(configSource);
+public class MyConfiguration implements HandlerInterceptor {
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+		//处理跨域问题
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST,PUT");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers",
+				"Origin, X-Requested-With, Content-Type, Accept");
+		return true;
 	}
 }
 
