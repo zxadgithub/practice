bbae1502a41c17701172cbb46d853a7368ffa7e6
xin_anzhang <zxadgg@gmail.com>
2019-01-28 09:41:12 +0800

    跨域

--- shortcutkey/src/main/java/com/zxa/shortcut/config/MyHandlerInterceptor.java
+++ shortcutkey/src/main/java/com/zxa/shortcut/config/MyHandlerInterceptor.java
@@ -1,5 +1,7 @@
 package com.zxa.shortcut.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,18 +18,21 @@ import javax.servlet.http.HttpServletResponse;
  */
 @Configuration
 public class MyHandlerInterceptor implements HandlerInterceptor {
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 		//处理跨域问题
-		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers",
 				request.getHeader("Access-Control-Request-Headers"));
+		logger.info("跨域请求" + request.getContextPath() + "返回值" + response.getHeaderNames());
 		if(request.getMethod().equals(RequestMethod.OPTIONS.name())) {
 			response.setStatus(HttpStatus.OK.value());
 			return false;
 		}
+
 		return true;
 	}
 }
