bf488dd6db79f83e68f770b2565553bab471c60c
zhangxinan <zhangxinan@maoyan.com>
2021-03-13 17:10:45 +0800

    链表是否相交

--- leetcode/src/main/java/com/zxa/practice/leetcode/link/ListNode.java
+++ leetcode/src/main/java/com/zxa/practice/leetcode/link/ListNode.java
@@ -12,4 +12,16 @@ public class ListNode {
     ListNode() {}
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public static ListNode build(String s){
+        String[] split = s.split(",");
+        ListNode head = new ListNode(Integer.parseInt(split[0]), null);
+        ListNode cur = head;
+        for (int i = 1; i < split.length; i++) {
+            ListNode listNode = new ListNode(Integer.parseInt(split[i]), null);
+            cur.next = listNode;
+            cur = listNode;
+        }
+        return head;
+    }
 }
