e2e7c7cfd0273e88030332fc3be90bb1308f2291
zhangxinan <zxadgg@gmail.com>
2021-04-07 21:46:11 +0800

    青蛙跳台阶2

--- leetcode/src/main/java/com/zxa/practice/leetcode/dp/NumWays.java
+++ leetcode/src/main/java/com/zxa/practice/leetcode/dp/NumWays.java
@@ -7,6 +7,11 @@ package com.zxa.practice.leetcode.dp;
  */
 public class NumWays {
 
+    /**
+     * 数组存储每次结果
+     * @param n
+     * @return
+     */
     public int numWays(int n) {
         if(n == 0){
             return 1;
@@ -25,4 +30,31 @@ public class NumWays {
         }
         return res[n - 1] % 1000000007;
     }
+
+    /**
+     * 只存储最近两次结果
+     * @param n
+     * @return
+     */
+    public int numWays2(int n) {
+        if(n == 0){
+            return 1;
+        }
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 2;
+        }
+        int a = 1;
+        int b = 2;
+        int sum = 0;
+        for(int i = 2; i< n; i++){
+            sum = (a % 1000000007) + (b % 1000000007);
+            a = b;
+            b = sum;
+        }
+        return sum % 1000000007;
+    }
+
 }
