e8c1b223de28390717d2c898db0e519532d5eb9b
zhangxinan <zxadgg@gmail.com>
2021-04-12 22:16:30 +0800

    数组中的逆序对

--- leetcode/src/main/java/com/zxa/practice/leetcode/sort/MergeSort.java
+++ leetcode/src/main/java/com/zxa/practice/leetcode/sort/MergeSort.java
@@ -16,26 +16,46 @@ public class MergeSort {
 
     public static void main(String[] args) {
         MergeSort mergeSort = new MergeSort();
-        Integer[] integers = new Integer[]{2,3,1,4,8,2,4,1,5};
+        int[] integers = new int[]{2,3,1,4,8,2,4,1,5};
         mergeSort.sort(integers);
         System.out.println(integers);
 
     }
 
-    public void sort(Integer[] nums){
+    public void sort(int[] nums){
         temp =  new int[nums.length];;
         mergeSort(nums, 0 ,nums.length - 1);
     }
 
-    private void mergeSort(Integer[] nums, int lo, int hi) {
+    private void mergeSort(int[] nums, int lo, int hi) {
         if (lo >= hi){
             return;
         }
         int mid = lo + ((hi - lo) >> 1);
         mergeSort(nums,lo, mid);
         mergeSort(nums, mid + 1, hi);
-        merge(nums, lo,mid, hi);
+        merge1(nums, lo,mid, hi);
     }
+
+    private void merge1(int[] nums, int l,int mid, int r) {
+        int[] temp = new int[nums.length];
+        for (int i = l; i < r + 1; i++) {
+            temp[i] = nums[i];
+        }
+        int i = l, j = mid + 1, k = l;
+        while (k <= r){
+            if (i > mid){
+                nums[k++] = temp[j++];
+            } else if (j > r){
+                nums[k++] = temp[i++];
+            } else if (temp[i] > temp[j]){
+                nums[k++] = temp[j++];
+            } else {
+                nums[k++] = temp[i++];
+            }
+        }
+    }
+
 
     private void merge(Integer[] nums, int lo,int mid, int hi) {
         int i = lo;
