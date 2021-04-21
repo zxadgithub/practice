8c593860657d9df55e56ea1fc23b9d17baa2deb6
zhangxinan <zxadgg@gmail.com>
2021-04-21 22:42:43 +0800

    three sum 左右指针

--- leetcode/src/main/java/com/zxa/practice/leetcode/number/ThreeSum.java
+++ leetcode/src/main/java/com/zxa/practice/leetcode/number/ThreeSum.java
@@ -19,6 +19,37 @@ public class ThreeSum {
         threeSum.threeSum(new int[]{-1,0,1,2,-1,-4});
     }
 
+    public List<List<Integer>> threeSum1(int[] nums) {
+        List<List<Integer>> result= new ArrayList<>();
+        Arrays.sort(nums);
+        int length = nums.length;
+        for (int i = 0; i< length - 2; i++){
+            if (i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int target = -nums[i];
+            int left = i + 1, right = length - 1;
+            while (left < right){
+                if (nums[left] + nums[right] == target){
+                    result.add(new ArrayList<>(Arrays.asList(nums[i], nums[left],nums[right])));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]){
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]){
+                        right--;
+                    }
+                } else if (nums[left] + nums[right] < target){
+                    left++;
+                }else if (nums[left] + nums[right] > target){
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result= new ArrayList<>();
         Arrays.sort(nums);
