7f171f070e503d00bd401d6bb3f4c8770b3741f8
zhangxinan <zxadgg@gmail.com>
2021-04-21 21:31:33 +0800

    最小覆盖子串

--- leetcode/src/main/java/com/zxa/practice/leetcode/string/MinWindow.java
+++ leetcode/src/main/java/com/zxa/practice/leetcode/string/MinWindow.java
@@ -3,6 +3,7 @@ package com.zxa.practice.leetcode.string;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -32,6 +33,55 @@ import java.util.Set;
  */
 public class MinWindow {
 
+
+    Map<Character, Integer> ori = new HashMap<Character, Integer>();
+    Map<Character, Integer> cnt = new HashMap<Character, Integer>();
+
+    public String minWindow1(String s, String t) {
+        int tLen = t.length();
+        for (int i = 0; i < tLen; i++) {
+            char c = t.charAt(i);
+            ori.put(c, ori.getOrDefault(c, 0) + 1);
+        }
+        int l = 0, r = -1;
+        int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
+        int sLen = s.length();
+        while (r < sLen) {
+            ++r;
+            if (r < sLen && ori.containsKey(s.charAt(r))) {
+                cnt.put(s.charAt(r), cnt.getOrDefault(s.charAt(r), 0) + 1);
+            }
+            while (check() && l <= r) {
+                if (r - l + 1 < len) {
+                    len = r - l + 1;
+                    ansL = l;
+                    ansR = l + len;
+                }
+                if (ori.containsKey(s.charAt(l))) {
+                    cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0) - 1);
+                }
+                ++l;
+            }
+        }
+        return ansL == -1 ? "" : s.substring(ansL, ansR);
+    }
+
+    public boolean check() {
+
+        Iterator iter = ori.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+            Character key = (Character) entry.getKey();
+            Integer val = (Integer) entry.getValue();
+            if (cnt.getOrDefault(key, 0) < val) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
     public static final String BLANK = " ";
 
     public static void main(String[] args) {
@@ -49,7 +99,7 @@ public class MinWindow {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
         List<Character> tempList = new ArrayList<>();
-        ;
+
         List<Character> result = new ArrayList<>();
         ;
 
