58bd865227412c9d84bc8109005e4cd428070c2d
zhangxinan <zxadgg@gmail.com>
2021-04-23 10:26:25 +0800

    二叉树后续遍历 递归

--- leetcode/src/main/java/com/zxa/practice/leetcode/tree/PostorderTraversal.java
+++ leetcode/src/main/java/com/zxa/practice/leetcode/tree/PostorderTraversal.java
@@ -1,7 +1,10 @@
 package com.zxa.practice.leetcode.tree;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author zhangxinan
@@ -19,6 +22,33 @@ public class PostorderTraversal {
         postorderTraversal0(root, res);
         return res;
     }
+
+    public List<Integer> postorderTraversal0(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        TreeNode prev = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.right == null || root.right == prev) {
+                res.add(root.val);
+                prev = root;
+                root = null;
+            } else {
+                stack.push(root);
+                root = root.right;
+            }
+        }
+        return res;
+    }
+
 
     public void postorderTraversal0(TreeNode root,List<Integer> res) {
         if(root == null){
