8f65a0bd2ef232c7da77f4df17090ed97beee031
zhangxinan <zxadgg@gmail.com>
2021-04-24 21:01:33 +0800

    去除重复字符串

---
+++ leetcode/src/main/java/com/zxa/practice/leetcode/string/RemoveDuplicates.java
@@ -0,0 +1,40 @@
+package com.zxa.practice.leetcode.string;
+
+import java.util.Stack;
+
+/**
+ * @author zhangxinan
+ * @Classname s
+ * @Date 2021/4/24 8:58 下午
+ */
+public class RemoveDuplicates {
+    public String removeDuplicates(String S) {
+        if (S == null || S.length() <= 1) {
+            return S;
+        }
+        Stack<Character> stack = new Stack<>();
+        stack.push(S.charAt(0));
+        for (int i = 1; i < S.length(); i++) {
+            Character c = S.charAt(i);
+            if (!stack.isEmpty()) {
+
+                Character top = stack.peek();
+                if (top == c) {
+                    stack.pop();
+                }else{
+                    stack.push(c);
+                }
+            } else{
+                stack.push(c);
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (Character c : stack) {
+            res.append(c.toString());
+        }
+        return res.toString();
+
+
+    }
+}
