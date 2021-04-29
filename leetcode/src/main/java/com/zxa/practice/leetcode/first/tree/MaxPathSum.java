ce502ec6351c33dc0c36b34aa81550a8d6ed8368
zhangxinan <zxadgg@gmail.com>
2021-04-29 21:53:53 +0800

    二叉树最长路径

---
+++ leetcode/src/main/java/com/zxa/practice/leetcode/first/tree/MaxPathSum.java
@@ -0,0 +1,55 @@
+package com.zxa.practice.leetcode.first.tree;
+
+import lombok.val;
+import org.apache.commons.lang3.math.NumberUtils;
+
+/**
+ * @author zhangxinan
+ * @Classname MaxLength
+ * @Date 2021/4/29 4:13 下午
+ * 路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不一定经过根节点。
+ *
+ * 路径和 是路径中各节点值的总和。
+ *
+ * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-maximum-path-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MaxPathSum {
+
+    int maxSum = Integer.MIN_VALUE;
+
+    public static void main(String[] args) {
+        MaxPathSum maxLength = new MaxPathSum();
+        maxLength.maxPathSum(new TreeNode(-3));
+    }
+
+    public int maxPathSum(TreeNode node){
+        maxGain(node);
+        return maxSum;
+    }
+
+    public int maxGain(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        // 递归计算左右子节点的最大贡献值
+        // 只有在最大贡献值大于 0 时，才会选取对应子节点
+        int leftGain = Math.max(maxGain(node.left), 0);
+        int rightGain = Math.max(maxGain(node.right), 0);
+
+        // 节点的最大路径和取决于该节点的值与该节点的左右子节点的最大贡献值
+        int priceNewpath = node.val + leftGain + rightGain;
+
+        // 更新答案
+        maxSum = Math.max(maxSum, priceNewpath);
+
+        // 返回节点的最大贡献值
+        return node.val + Math.max(leftGain, rightGain);
+    }
+
+
+}
