c0c47c3da55cbdaeccf10a4e1cf63e9105d0e9c6
zhangxinan <zxadgg@gmail.com>
2021-05-18 20:54:52 +0800

    N反转链表递归

--- leetcode/src/main/java/com/zxa/practice/leetcode/first/link/ReverseKGroup.java
+++ leetcode/src/main/java/com/zxa/practice/leetcode/first/link/ReverseKGroup.java
@@ -37,6 +37,33 @@ public class ReverseKGroup {
         return newHead;
     }
 
+    public ListNode reverseKGroup0(ListNode head, int k) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode dump = new ListNode(-1);
+        dump.next = head;
+        ListNode pre = dump, end = dump;
+        while(end != null) {
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.next;
+            }
+            if (end == null){
+                break;
+            }
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+
+            pre.next = reverse(start, null);
+            start.next = next;
+            pre = start;
+            end = pre;
+        }
+        return dump.next;
+    }
+
     private ListNode reverse(ListNode head, ListNode end) {
         if (head == null) {
             return head;
